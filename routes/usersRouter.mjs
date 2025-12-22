9995a2b4444e9c83551f36b15cb569d843b7f9b5
santiago-404 <asantiago.9861@umak.edu.ph>
2025-12-22 11:07:43 +0800

    making sure

--- routes/usersRouter.mjs
+++ routes/usersRouter.mjs
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { books } from "../books.mjs";
 import { users } from "../users.mjs";
 import findIdIndex from "../middlewares/findIdIndex.mjs";
 
@@ -9,15 +10,31 @@ const router = Router();
 
 //only send the main details such as id and name
 //because thats the only think the users see when they look at thei profile tab
-//only fetch the favourites data when they click on the favourites tab
-//to make it efficient
+
 router.get('/:id', findIdIndex(users), (req, res) => {
   let user = {id: req.validatedId, username: users[req.dataIndex].userName}
   return res.send(user);
 })
 
+//only fetch the favourites data when they click on the favourites tab
+//to make it efficient
 router.get('/:id/favourites', findIdIndex(users), (req, res) => {
   return res.send(users[req.dataIndex].favourites);
+})
+
+router.post('/:id/favourites', findIdIndex(users), (req, res) => {
+  const validatedBookId = books.findIndex(book => book.id === req.body.id);
+  if (validatedBookId === -1) return res.sendStatus(404);
+  users[req.dataIndex].favourites.push(validatedBookId);
+  return res.send(users[req.dataIndex].favourites);
+})
+
+router.delete('/:id/favourites', findIdIndex(users), (req, res) => {
+  const userIndex = req.dataIndex;
+  const bookIndexInFavourites = users[userIndex].favourites.findIndex(id => id === req.body.id);
+  if (bookIndexInFavourites === -1) return res.sendStatus(404);
+  users[req.dataIndex].favourites.splice(bookIndexInFavourites ,1);
+  return res.send(users[req.dataIndex]);
 })
 
 export default router;
