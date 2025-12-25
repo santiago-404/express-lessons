522f3f1a8892b47e8713e027003e4187b116e23e
santiago-404 <asantiago.9861@umak.edu.ph>
2025-12-25 12:39:42 +0800

    learned sessions and cookies, made a sample auth process

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,39 @@
 import express from 'express';
 import bookRouter from '../routes/booksRouter.mjs';
 import userRouter from '../routes/usersRouter.mjs';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import { users } from '../users.mjs';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+  secret: 'why chicken',
+  saveUninitialized: false,
+  resave: false,
+  cookie:{
+    maxAge: 60000 * 60
+  }
+}))
+
 app.use('/api/books' ,bookRouter)
-app.use('/api/users' ,userRouter)
+app.use('/api/user' ,userRouter)
+
+app.post('/api/login', (req, res) => {
+  const {body: USER} = req;
+  if(req.session.user) return res.send('Logged in');
+  if(!USER) return res.sendStatus(400);
+  const findUser = users.find(user => user.userName === USER.userName);
+  if(!findUser || USER.password !== findUser.password) return res.sendStatus(401);
+  req.session.user = findUser;
+  console.log(req.session.id);
+  return res.send('Logged in');
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on Port: ${PORT}`);  
--- users.mjs
+++ users.mjs
@@ -1,5 +1,5 @@
 export let users = [
-  {id: 1, userName: 'lily', favourites: [0, 1]},
-  {id: 2, userName: 'ali', favourites: [2]},
-  {id: 3, userName: 'abe', favourites: []}
+  {id: 1, userName: 'lily', password: 'abc123', favourites: [1]},
+  {id: 2, userName: 'ali', password: 'abc124', favourites: [2]},
+  {id: 3, userName: 'abe', password: 'abc125', favourites: []}
 ]
